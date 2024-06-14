396bc3471241bbb0349a3379a9bbff25d0633940
Mahmoud Hamdy <148990144+mahmoodhamdi@users.noreply.github.com>
2024-06-14 06:03:51 +0300

    first commit

---
+++ lib/task_manager_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+// TaskManagementApp>> task_manager_app.dart
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+    );
+  }
+}
